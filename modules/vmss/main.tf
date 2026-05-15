92601d33c497298ff742958e4217c48d802b62a6
Raghu K <raghuk.vit@gmail.com>
2026-05-15 07:05:38 +0530

    RoboShop Code

--- modules/vmss/main.tf
+++ modules/vmss/main.tf
@@ -30,4 +30,65 @@ resource "azurerm_linux_virtual_machine_scale_set" "main" {
       subnet_id = var.subnet_id
     }
   }
+
+  lifecycle {
+    ignore_changes = [instances]
+  }
+}
+
+resource "azurerm_monitor_autoscale_setting" "main" {
+  name                = "${var.component_name}-${var.env}-autoscale"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  target_resource_id  = azurerm_linux_virtual_machine_scale_set.main.id
+
+  profile {
+    name = "default"
+
+    capacity {
+      default = 2
+      minimum = 2
+      maximum = 5
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.main.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 70
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.main.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 30
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+  }
 }
