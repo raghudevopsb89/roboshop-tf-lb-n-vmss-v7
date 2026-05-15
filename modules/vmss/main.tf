efd32c167d2358382713ec020be0c255ea47c29f
Raghu K <raghuk.vit@gmail.com>
2026-05-15 07:00:54 +0530

    RoboShop Code

--- modules/vmss/main.tf
+++ modules/vmss/main.tf
@@ -1,42 +1,33 @@
-resource "azurerm_virtual_machine_scale_set" "main" {
+resource "azurerm_linux_virtual_machine_scale_set" "main" {
   name                = "${var.component_name}-${var.env}"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
-  upgrade_policy_mode = "Rolling"
 
-  sku {
-    name     = "Standard_B1s"
-    tier     = "Standard"
-    capacity = 2
+  sku            = "Standard_B1s"
+  instances      = 2
+  admin_username = "devops"
+  admin_password = "DevOps@123456"
+
+  disable_password_authentication = false
+
+  source_image_id = var.image_id
+
+  secure_boot_enabled = true
+  vtpm_enabled        = true
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
-  storage_profile_os_disk {
-    name          = "osDiskProfile"
-    caching       = "ReadWrite"
-    create_option = "FromImage"
-    image         = var.image_id
-    os_type       = "Linux"
-  }
-
-  os_profile {
-    computer_name_prefix = "${var.component_name}-${var.env}"
-    admin_username       = "devops"
-    admin_password       = "DevOps@123456"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  network_profile {
-    name    = "${var.component_name}-${var.env}"
+  network_interface {
+    name    = "${var.component_name}-${var.env}-nic"
     primary = true
 
     ip_configuration {
-      name      = "${var.component_name}-${var.env}"
+      name      = "internal"
       primary   = true
       subnet_id = var.subnet_id
     }
   }
-
 }
